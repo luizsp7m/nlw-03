b5b3f120903e30c01040bb65a83c67a6d6cabb65
Luiz Antonio <luizoliveira2808@gmail.com>
2020-10-18 21:10:59 -0300

    Quinta aula

---
+++ backend/src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import './database/connection';
+import 'express-async-errors';
+import errorHandler from './errors/handler';
+import path from 'path';
+import routes from './routes';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler);
+app.listen(3333);
